db21a53522f7389545896e8aae8c8af1057850d9
Raunak Rej <55279085+RaunakRej@users.noreply.github.com>
2026-09-12 11:19:26 +0530

    Add files via upload

--- src/main/java/com/raunak/youtube/api/model/Video.java
+++ src/main/java/com/raunak/youtube/api/model/Video.java
@@ -1,8 +1,19 @@
 package com.raunak.youtube.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "videos")
 public class Video {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String title;
     private String channel;
     private String views;
